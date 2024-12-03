716431abfa5a59806002473efea1e659fd9856f4
nguyenhuy2311 <129100183+nguyenhuy2311@users.noreply.github.com>
2024-12-03 22:34:42 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       proxy: {
+        "/ws": {
+          target: "ws://localhost:3000",
+          ws: true,
+        },
         "/api": {
           target: "http://localhost:3000",
           changeOrigin: true,
