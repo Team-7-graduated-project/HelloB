af79048c59295475e1ee17e83e403c2e7141e830
nguyenhuy2311 <nguyenhuy23112004@gmail.com>
2024-12-15 14:45:56 +0700

    modify some file

--- api/index.js
+++ api/index.js
@@ -82,7 +82,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://hello-b.vercel.app", "https://clientt-g7c3.onrender.com"],
+    origin: ["http://localhost:5173", "https://hello-b.vercel.app", "https://clientt-g7c3.onrender.com","https://hello-b.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
